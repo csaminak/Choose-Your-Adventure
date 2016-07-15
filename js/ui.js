ff575ef8a2b6982df173c326a3ee5dfe30651ccd
Samina Chhatwal <csaminak@gmail.com>
2016-07-15 15:18:45 -0400

    added view for different adventure options

--- js/ui.js
+++ js/ui.js
@@ -16,15 +16,44 @@
     $loginForm.on('submit', function signIn(event){
         event.preventDefault();
         ns.login($user.val())
-            .done(function displayStory() {
-                $storyView.show();
-                $storyStep.show();
-                $loginForm.hide();
-                $('.login p').hide();
-                $('.story-end').hide();
-            });
-
+            .done(storyList);
     });
+
+    $('ul').on('click', 'button', function (event){
+        if(event.target === '[data-id="1"]') {
+            console.log('button1');
+        } else if(event.target === '[data-id="2"]') {
+            console.log('button2');
+        } else if(event.target === '[data-id="3"]'){
+            console.log('button3');
+        }
+    });
+
+    /**
+     * when user logs in they will be directed to a view with different stories to pick.
+     * @return {void}
+     */
+    function storyList() {
+        $storyStep.hide();
+        $('.login').hide();
+        $storyView
+            .show()
+            .find('ul')
+                .append('<li class="adventure">\
+                            <h2>Beach Vacation</h2>\
+                            <button data-id="1">Begin Adventure</button>\
+                         </li>\
+                         <li class="adventure">\
+                            <h2>Camping Trip</h2>\
+                            <button data-id="2">Begin Adventure</button>\
+                         </li>\
+                         <li class="adventure">\
+                            <h2>Hiking in the Mountains</h2>\
+                            <button data-id="3">Begin Adventure</button>\
+                         </li>');
+        console.log($('ul'));
+    }
+
 
 
 
